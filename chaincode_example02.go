481ce993406d1746b581b563ecdfbf3bc7ae5b2b
vishnuvzerone <vishnuvzerone@gmail.com>
2017-07-13 16:08:35 +0530

    Update chaincode_example02.go

--- chaincode_example02.go
+++ chaincode_example02.go
@@ -112,6 +112,27 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	return valAsbytes, nil
 }
 
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
+// 
+// AddUser - Add New User
+// 
 func (t *SimpleChaincode) AddUser(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, name, password, propertyid, info string
 	var err error
@@ -164,20 +185,4 @@ func (t *SimpleChaincode) ReadUserName(stub shim.ChaincodeStubInterface, args []
 	return []byte(usr.Name), nil
 }
 
-// read - query function to read key/value pair
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err error
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-	}
 
-	key = args[0]
-	valAsbytes, err := stub.GetState(key)
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	return valAsbytes, nil
-}
