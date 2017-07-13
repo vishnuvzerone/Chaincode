e79a74adfd662c57d218fbfb380e477c1a97b082
vishnuvzerone <vishnuvzerone@gmail.com>
2017-07-13 11:29:14 +0530

    Update chaincode_example02.go

--- chaincode_example02.go
+++ chaincode_example02.go
@@ -93,20 +93,21 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 }
 
 // read - query function to read key/value pair
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
-
+	var readstring
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
 	key = args[0]
 	valAsbytes, err := stub.GetState(key)
+	readstring = string(valAsbytes)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	return string(valAsbytes), ""
+	return readstring, nil
 }
