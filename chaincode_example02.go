dd10e21b09968b3fb84ebb167de8a6aef13e3462
vishnuvzerone <vishnuvzerone@gmail.com>
2017-07-13 12:00:25 +0530

    Update chaincode_example02.go

--- chaincode_example02.go
+++ chaincode_example02.go
@@ -16,6 +16,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"encoding/json"
 	
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -78,7 +79,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	var key, value string
 	var err error
 	fmt.Println("running write()")
-
+	bytes, err := json.Marshal(args[1])
 	if len(args) != 2 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
 	}
