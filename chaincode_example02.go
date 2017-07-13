c73365089ed47fa2bb27f008d1554ec067362729
vishnuvzerone <vishnuvzerone@gmail.com>
2017-07-13 11:31:16 +0530

    Update chaincode_example02.go

--- chaincode_example02.go
+++ chaincode_example02.go
@@ -96,7 +96,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
-	var readstring
+	var readstring, jsonResp string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
